94b44316509787000c1fab0aa111378eea3ed3d3
Jhonny Izidoro Menarim <jhonnymenarim@ecompleto.com.br>
2020-09-23 12:53:04 -0300

    Link WhatsApp

--- resources/js/modules/Social.js
+++ resources/js/modules/Social.js
@@ -16,7 +16,7 @@ export const initShareButtons = () => {
 
 export const setWhatsAppLink = () => {
 	const links = document.querySelectorAll('a[data-whatsapp]')
-	const endpoint = window.innerWidth < 1000 ? 'https://api.whatsapp.cm' : 'https://web.whatsapp.com'
+	const endpoint = window.innerWidth < 1000 ? 'https://api.whatsapp.com' : 'https://web.whatsapp.com'
 	const text = encodeURIComponent(`(${window.location.href}) Olá, vi você no Matriarcal e gostaria de mais informações.`)
 	links.forEach(link => link.href = `${endpoint}/send?phone=55${link.dataset.whatsapp}&text=${text}`)
 }
